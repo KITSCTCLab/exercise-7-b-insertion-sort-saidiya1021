7ebd919149e7054d9632ab58d0682cdc11690d4a
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-09-18 22:56:14 +0530

    commit5

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ def insertionSort(array) -> List[int]:
       while j>=0 and min<array[j]:
         array[j+1]=array[j]
         j-=1
-      array[j+1]=key
+      array[j+1]=min
   return array
 
 # data = [9, 5, 1, 4, 3]
